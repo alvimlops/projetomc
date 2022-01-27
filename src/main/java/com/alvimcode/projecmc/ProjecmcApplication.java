be7ed925b387e12fb0c60ab8ed1715f6bc71994b
Alvaro Rodrigues <alvim.lopez@gmail.com>
2022-01-27 14:46:57 -0300

    Projeto criado

---
+++ src/main/java/com/alvimcode/projecmc/ProjecmcApplication.java
@@ -0,0 +1,13 @@
+package com.alvimcode.projecmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvimcode/projecmc/ProjecmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvimcode.projecmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
