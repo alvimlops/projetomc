3194b79f58d1ce3440ea2b9dbaa3c0f486cb6931
Alvaro Rodrigues <alvim.lopez@gmail.com>
2022-02-02 18:39:16 -0300

    Criando operação de Instanciação

--- src/main/java/com/alvimcode/projecmc/ProjecmcApplication.java
+++ src/main/java/com/alvimcode/projecmc/ProjecmcApplication.java
@@ -1,13 +1,33 @@
 package com.alvimcode.projecmc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjecmcApplication {
+import com.alvimcode.projecmc.domain.Categoria;
+import com.alvimcode.projecmc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjecmcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjecmcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritotio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
