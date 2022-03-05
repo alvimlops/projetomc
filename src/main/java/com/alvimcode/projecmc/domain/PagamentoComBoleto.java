6355f7db158d5c08ee74066b097c7c606f77ae41
alvaro.rodrigues <alvaro.rodrigues@mvrec.local>
2022-03-05 18:06:52 -0300

    Add pedido, estadoPagamento e Pagamanto, um para um

---
+++ src/main/java/com/alvimcode/projecmc/domain/PagamentoComBoleto.java
@@ -0,0 +1,5 @@
+package com.alvimcode.projecmc.domain;
+
+public class PagamentoComBoleto {
+
+}
---
+++ src/main/java/com/alvimcode/projecmc/domain/Pedido.java
@@ -0,0 +1,10 @@
+package com.alvimcode.projecmc.domain;
+
+import java.util.Date;
+
+public class Pedido {
+	
+	private Integer id;
+	private Date instante;
+
+}
