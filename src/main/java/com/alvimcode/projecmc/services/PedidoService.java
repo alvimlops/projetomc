7476c3ec62ee15699f17a4ea484e084ce55d06ea
alvaro.rodrigues <alvaro.rodrigues@mvrec.local>
2022-03-05 23:38:12 -0300

    Endpoint pedidos {id} disponibilizado

--- src/main/java/com/alvimcode/projecmc/services/PedidoService.java
+++ src/main/java/com/alvimcode/projecmc/services/PedidoService.java
@@ -5,23 +5,23 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.alvimcode.projecmc.domain.Categoria;
+import com.alvimcode.projecmc.domain.Cliente;
 import com.alvimcode.projecmc.domain.Pedido;
-import com.alvimcode.projecmc.repositories.CategoriaRepository;
+import com.alvimcode.projecmc.repositories.ClienteRepository;
 import com.alvimcode.projecmc.services.exception.ObjectNotFoundException;
 
 @Service
-public class CategoriaService {
+public class PedidoService {
 	
 	//Estancia automaticamente pelo Spring, usando esse anotation
 	@Autowired
-	private CategoriaRepository repo;
+	private ClienteRepository repo;
 	
 	//Operação capaz de busca uma categoria por codigo.
-	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = Optional.empty();
 		if(obj == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", tipo: " + Categoria.class.getName());
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", tipo: " + Cliente.class.getName());
 		}	
 			
 			
